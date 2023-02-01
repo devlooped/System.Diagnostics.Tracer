af1161e98aa7888c35dea4b4b588a8a2d8b8b7bc
Daniel Cazzulino <daniel@cazzulino.com>
2023-02-01 00:25:35 +0000

    Apply dotnet format

--- src/System.Diagnostics.Tracer.Tests/StartActivityTests.cs
+++ src/System.Diagnostics.Tracer.Tests/StartActivityTests.cs
@@ -11,156 +11,166 @@ using Xunit;
 
 namespace System.Diagnostics
 {
-	public class StartActivityTests
-	{
-		[Fact]
-		public void when_using_activity_then_starts_and_ends ()
-		{
-			var listener = new Mock<TraceListener>();
-			Tracer.Configuration.AddListener ("Foo", listener.Object);
-			Tracer.Configuration.SetTracingLevel ("Foo", SourceLevels.All);
+    public class StartActivityTests
+    {
+        [Fact]
+        public void when_using_activity_then_starts_and_ends()
+        {
+            var listener = new Mock<TraceListener>();
+            Tracer.Configuration.AddListener("Foo", listener.Object);
+            Tracer.Configuration.SetTracingLevel("Foo", SourceLevels.All);
 
-			var originalId = Guid.NewGuid();
-			Trace.CorrelationManager.ActivityId = originalId;
+            var originalId = Guid.NewGuid();
+            Trace.CorrelationManager.ActivityId = originalId;
 
-			var tracer = Tracer.Get("Foo");
-			var activityId = Guid.Empty;
+            var tracer = Tracer.Get("Foo");
+            var activityId = Guid.Empty;
 
-			using (var activity = tracer.StartActivity("Test")) {
-				activityId = Trace.CorrelationManager.ActivityId;
-			}
+            using (var activity = tracer.StartActivity("Test"))
+            {
+                activityId = Trace.CorrelationManager.ActivityId;
+            }
 
-			Assert.NotEqual (Guid.Empty, activityId);
+            Assert.NotEqual(Guid.Empty, activityId);
 
-			// Activity equals:
-			//	 Transfer -> Data(Start) -> [Event] -> Data(Stop) -> Transfer
+            // Activity equals:
+            //	 Transfer -> Data(Start) -> [Event] -> Data(Stop) -> Transfer
 
-			listener.Verify (x => x.TraceTransfer (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				It.IsAny<int> (),
-				It.IsAny<string> (),
-				activityId));
+            listener.Verify(x => x.TraceTransfer(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                It.IsAny<int>(),
+                It.IsAny<string>(),
+                activityId));
 
-			listener.Verify (x => x.TraceEvent (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				TraceEventType.Start,
-				It.IsAny<int> (),
-				It.IsAny<string>()));
+            listener.Verify(x => x.TraceEvent(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                TraceEventType.Start,
+                It.IsAny<int>(),
+                It.IsAny<string>()));
 
-			listener.Verify (x => x.TraceEvent (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				TraceEventType.Stop,
-				It.IsAny<int> (),
-				It.IsAny<string> ()));
+            listener.Verify(x => x.TraceEvent(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                TraceEventType.Stop,
+                It.IsAny<int>(),
+                It.IsAny<string>()));
 
-			listener.Verify (x => x.TraceTransfer (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				It.IsAny<int> (),
-				It.IsAny<string> (),
-				originalId));
-		}
+            listener.Verify(x => x.TraceTransfer(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                It.IsAny<int>(),
+                It.IsAny<string>(),
+                originalId));
+        }
 
-		[Fact]
-		public void when_no_initial_activity_then_does_not_transfer ()
-		{
-			var listener = new Mock<TraceListener>();
-			Tracer.Configuration.AddListener ("Foo", listener.Object);
-			Tracer.Configuration.SetTracingLevel ("Foo", SourceLevels.All);
+        [Fact]
+        public void when_no_initial_activity_then_does_not_transfer()
+        {
+            var listener = new Mock<TraceListener>();
+            Tracer.Configuration.AddListener("Foo", listener.Object);
+            Tracer.Configuration.SetTracingLevel("Foo", SourceLevels.All);
 
-			Trace.CorrelationManager.ActivityId = Guid.Empty;
+            Trace.CorrelationManager.ActivityId = Guid.Empty;
 
-			var tracer = Tracer.Get("Foo");
-			var activityId = Guid.Empty;
+            var tracer = Tracer.Get("Foo");
+            var activityId = Guid.Empty;
 
-			using (var activity = tracer.StartActivity ("Test")) {
-				activityId = Trace.CorrelationManager.ActivityId;
-			}
+            using (var activity = tracer.StartActivity("Test"))
+            {
+                activityId = Trace.CorrelationManager.ActivityId;
+            }
 
-			Assert.NotEqual (Guid.Empty, activityId);
+            Assert.NotEqual(Guid.Empty, activityId);
 
-			// Activity without existing activity in progress equals:
-			//	 Data(Start) -> [Event] -> Data(Stop)
+            // Activity without existing activity in progress equals:
+            //	 Data(Start) -> [Event] -> Data(Stop)
 
-			listener.Verify (x => x.TraceEvent (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				TraceEventType.Start,
-				It.IsAny<int> (),
-				It.IsAny<string>()));
+            listener.Verify(x => x.TraceEvent(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                TraceEventType.Start,
+                It.IsAny<int>(),
+                It.IsAny<string>()));
 
-			listener.Verify (x => x.TraceEvent (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				TraceEventType.Stop,
-				It.IsAny<int> (),
-				It.IsAny<string> ()));
+            listener.Verify(x => x.TraceEvent(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                TraceEventType.Stop,
+                It.IsAny<int>(),
+                It.IsAny<string>()));
 
-			listener.Verify (x => x.TraceTransfer (
-				It.IsAny<TraceEventCache> (),
-				"Foo",
-				It.IsAny<int> (),
-				It.IsAny<string> (),
-				It.IsAny<Guid>()),
-				Times.Never ());
-		}
+            listener.Verify(x => x.TraceTransfer(
+                It.IsAny<TraceEventCache>(),
+                "Foo",
+                It.IsAny<int>(),
+                It.IsAny<string>(),
+                It.IsAny<Guid>()),
+                Times.Never());
+        }
 
-		[Fact]
-		public void when_activity_has_exception_then_traces_data ()
-		{
-			var xmlFile = Path.GetTempFileName();
-			var listener = new XmlWriterTraceListener(xmlFile);
+        [Fact]
+        public void when_activity_has_exception_then_traces_data()
+        {
+            var xmlFile = Path.GetTempFileName();
+            var listener = new XmlWriterTraceListener(xmlFile);
 
-			Tracer.Configuration.AddListener ("Foo", listener);
-			Tracer.Configuration.SetTracingLevel ("Foo", SourceLevels.All);
+            Tracer.Configuration.AddListener("Foo", listener);
+            Tracer.Configuration.SetTracingLevel("Foo", SourceLevels.All);
 
-			var tracer = Tracer.Get("Foo");
+            var tracer = Tracer.Get("Foo");
 
-			using (var outer = tracer.StartActivity ("Outer")) {
-				using (var inner = tracer.StartActivity("Inner")) {
-					tracer.Info ("Hey & > 23!");
-				}
+            using (var outer = tracer.StartActivity("Outer"))
+            {
+                using (var inner = tracer.StartActivity("Inner"))
+                {
+                    tracer.Info("Hey & > 23!");
+                }
 
-				try {
-					throw new ArgumentException ();
-				} catch (Exception ex) {
-					ex.Data.Add ("Version", "1.0");
-					ex.Data.Add ("Branch", "master");
-					ex.Data.Add ("Commit", "1ab314b");
-					tracer.Error (ex, "Some error!");
-				}
-			}
+                try
+                {
+                    throw new ArgumentException();
+                }
+                catch (Exception ex)
+                {
+                    ex.Data.Add("Version", "1.0");
+                    ex.Data.Add("Branch", "master");
+                    ex.Data.Add("Commit", "1ab314b");
+                    tracer.Error(ex, "Some error!");
+                }
+            }
 
-			Tracer.Configuration.RemoveListener ("Foo", listener);
-			listener.Flush ();
-			listener.Dispose ();
+            Tracer.Configuration.RemoveListener("Foo", listener);
+            listener.Flush();
+            listener.Dispose();
 
-			var xmlns = XNamespace.Get("http://schemas.microsoft.com/2004/10/E2ETraceEvent/TraceRecord");
-			var commitFound = false;
+            var xmlns = XNamespace.Get("http://schemas.microsoft.com/2004/10/E2ETraceEvent/TraceRecord");
+            var commitFound = false;
 
-			using (var reader = XmlReader.Create (xmlFile, new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment })) {
-				Assert.Equal (XmlNodeType.Element, reader.MoveToContent ());
-				do {
-					var xml = reader.ReadOuterXml();
-					var doc = XDocument.Load(new StringReader(xml));
-					var record = doc.Descendants(xmlns + "TraceRecord").FirstOrDefault();
-					if (record != null && record.Attribute("Severity") != null && record.Attribute("Severity").Value == "Error") {
-						commitFound = record.Element(xmlns + "DataItems")?.Element(xmlns + "Commit")?.Value == "1ab314b";
-					}
-				} while (!commitFound && !reader.EOF);
-			}
+            using (var reader = XmlReader.Create(xmlFile, new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment }))
+            {
+                Assert.Equal(XmlNodeType.Element, reader.MoveToContent());
+                do
+                {
+                    var xml = reader.ReadOuterXml();
+                    var doc = XDocument.Load(new StringReader(xml));
+                    var record = doc.Descendants(xmlns + "TraceRecord").FirstOrDefault();
+                    if (record != null && record.Attribute("Severity") != null && record.Attribute("Severity").Value == "Error")
+                    {
+                        commitFound = record.Element(xmlns + "DataItems")?.Element(xmlns + "Commit")?.Value == "1ab314b";
+                    }
+                } while (!commitFound && !reader.EOF);
+            }
 
-			Assert.True(commitFound, "Failed to find traced commit ID exception data.");
+            Assert.True(commitFound, "Failed to find traced commit ID exception data.");
 
-			//Process.Start ("notepad", xml);
+            //Process.Start ("notepad", xml);
 
-			//Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\SvcTraceViewer.exe", xml);
-			//Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\SvcTraceViewer.exe", xml);
-			//Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\SvcTraceViewer.exe", xml);
-			//Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\SvcTraceViewer.exe", xml);
-		}
-	}
+            //Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\SvcTraceViewer.exe", xml);
+            //Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\SvcTraceViewer.exe", xml);
+            //Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\SvcTraceViewer.exe", xml);
+            //Process.Start (@"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\SvcTraceViewer.exe", xml);
+        }
+    }
 }
--- src/System.Diagnostics.Tracer/DiagnosticsTracer.cs
+++ src/System.Diagnostics.Tracer/DiagnosticsTracer.cs
@@ -2,63 +2,63 @@
 
 namespace System.Diagnostics
 {
-	/// <summary>
-	/// Implements the <see cref="ITracer"/> interface on top of
-	/// <see cref="TraceSource"/>.
-	/// </summary>
-	class DiagnosticsTracer
-	{
-		TraceSource source;
+    /// <summary>
+    /// Implements the <see cref="ITracer"/> interface on top of
+    /// <see cref="TraceSource"/>.
+    /// </summary>
+    class DiagnosticsTracer
+    {
+        TraceSource source;
 
-		public DiagnosticsTracer(TraceSource source)
-		{
-			this.source = source;
-		}
+        public DiagnosticsTracer(TraceSource source)
+        {
+            this.source = source;
+        }
 
-		public void Trace(string sourceName, TraceEventType type, object message)
-		{
-			lock (source)
-			{
-				using (new SourceNameReplacer(source, sourceName))
-				{
-					// Portable version has no concept of a trace transfer.
-					source.TraceEvent(type, 0, message.ToString());
-				}
-			}
-		}
+        public void Trace(string sourceName, TraceEventType type, object message)
+        {
+            lock (source)
+            {
+                using (new SourceNameReplacer(source, sourceName))
+                {
+                    // Portable version has no concept of a trace transfer.
+                    source.TraceEvent(type, 0, message.ToString());
+                }
+            }
+        }
 
-		public void Trace(string sourceName, TraceEventType type, string format, params object[] args)
-		{
-			lock (source)
-			{
-				using (new SourceNameReplacer(source, sourceName))
-				{
-					if (args != null && args.Length > 0)
-						source.TraceEvent(type, 0, format, args);
-					else
-						source.TraceEvent(type, 0, format);
-				}
-			}
-		}
+        public void Trace(string sourceName, TraceEventType type, string format, params object[] args)
+        {
+            lock (source)
+            {
+                using (new SourceNameReplacer(source, sourceName))
+                {
+                    if (args != null && args.Length > 0)
+                        source.TraceEvent(type, 0, format, args);
+                    else
+                        source.TraceEvent(type, 0, format);
+                }
+            }
+        }
 
-		public void Trace(string sourceName, TraceEventType type, Exception exception, object message)
-		{
-			Trace(sourceName, type, exception, message.ToString());
-		}
+        public void Trace(string sourceName, TraceEventType type, Exception exception, object message)
+        {
+            Trace(sourceName, type, exception, message.ToString());
+        }
 
-		public void Trace(string sourceName, TraceEventType type, Exception exception, string format, params object[] args)
-		{
-			lock (source)
-			{
-				using (new SourceNameReplacer(source, sourceName))
-				{
-					var message = format;
-					if (args != null && args.Length > 0)
-						message = string.Format(CultureInfo.CurrentCulture, format, args);
+        public void Trace(string sourceName, TraceEventType type, Exception exception, string format, params object[] args)
+        {
+            lock (source)
+            {
+                using (new SourceNameReplacer(source, sourceName))
+                {
+                    var message = format;
+                    if (args != null && args.Length > 0)
+                        message = string.Format(CultureInfo.CurrentCulture, format, args);
 
-					source.TraceEvent(type, 0, message + Environment.NewLine + exception);
-				}
-			}
-		}
-	}
+                    source.TraceEvent(type, 0, message + Environment.NewLine + exception);
+                }
+            }
+        }
+    }
 }
